2461a40a959efdc13767c643a8f9246bbce88f25
PrtkPokhrel <pokhrelpratik52@gmail.com>
2024-03-26 13:59:59 +0545

    update

--- Week6/Sphere.java
+++ Week6/Sphere.java
@@ -5,12 +5,53 @@
 // -   Include a toString method that returns a one-line description of the sphere.
 // -   Create a driver class (main class) called MultiSphere, whose main method instantiates and updates several Sphere objects.
 
-package Week6;
-public class Sphere {
-    
+// V = 4/3 π r³
+// S=4 pie r squre
 
+package Week6;
+import java.util.Scanner;
+import java.lang.Math;
+public class Sphere {
+    private int sphereDiameter;
+
+    public Sphere() {
+        sphereDiameter=2 ;
+    }
+
+    public int getValue() {
+        return sphereDiameter;
+    }
+
+    public int setValue(int values) {
+        this.sphereDiameter = values;
+        return values;
+    }
+    public double result(int diameter,double pie ){
+        double radius=diameter/2.0;
+        return (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
+    }
+    public double surface(int diameter,double pie){
+        double radius = diameter / 2.0;
+        return 4 * Math.PI * Math.pow(radius, 2);
+
+    }
+
+    public String toString() {
+        return "Sphere diameter " + sphereDiameter + ", Volume: " + result(sphereDiameter, Math.PI) + ", Surface Area: " + surface(sphereDiameter, Math.PI);
+    }
+    
     public static void main(String[] args) {
-        
+ Scanner scan=new Scanner(System.in);
+ Sphere obj=new Sphere();
+ int temDiameter;
+ System.out.print("Enter the diameter of the sphere: ");
+ temDiameter=scan.nextInt();
+ double pie=Math.PI;
+ obj.setValue(temDiameter);
+ obj.result(temDiameter, pie);
+ obj.surface(temDiameter, pie);
+ System.out.println(obj.toString());
+
+ scan.close();
     }
 }
-
