c77711d40af64697ee96153a7240f9df6707c37b
PrtkPokhrel <pokhrelpratik52@gmail.com>
2024-03-26 08:41:23 +0545

    update

---
+++ Week6/Sphere.java
@@ -0,0 +1,16 @@
+// Design and implement a class called Sphere that contains instance data that represent a sphere’s diameter.
+// -   Define a Sphere constructor to accept and initialize the diameter.
+// -   Include getter and setter methods for the diameter.
+// -   Include methods to calculate and return the volume and surface area of the sphere.
+// -   Include a toString method that returns a one-line description of the sphere.
+// -   Create a driver class (main class) called MultiSphere, whose main method instantiates and updates several Sphere objects.
+
+package Week6;
+public class Sphere {
+    
+
+    public static void main(String[] args) {
+        
+    }
+}
+
